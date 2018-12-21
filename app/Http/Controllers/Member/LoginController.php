bb8760a0c6404d161818fbfd90326daf03960d2e
vagrant <vagrant@planstin2>
2018-12-21 18:53:36 +0000

    #2

--- app/Http/Controllers/Member/LoginController.php
+++ app/Http/Controllers/Member/LoginController.php
@@ -1,22 +1,23 @@
 <?php 
 namespace App\Http\Controllers\Member;
 
-use App\Http\Controllers\Controller; 
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request; 
 
 class LoginController extends Controller {
 
-    public function login(){
+    public function login(Request $request){
         return $this->view('member.login.login');
     }
-    public function forgotPassword(){
+    public function forgotPassword(Request $request){
         
         return $this->view('member.login.forgot');
     }
-    public function resetPassword(){
+    public function resetPassword(Request $request){
         
         return $this->view('member.login.pass-reset');
     }
-    public function recoveryCode(){
+    public function recoveryCode(Request $request){
        
        return $this->view('member.login.recovery-code');
     }
