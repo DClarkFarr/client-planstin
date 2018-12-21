b0abe98e1484c92092d9546468ff96323c4ae0d9
Daniel C Farr <danielcfarr@gmail.com>
2018-12-21 13:33:46 -0700

    getting ready for header & footer redesign

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,16 +82,7 @@
     </div>
 
 
-    @section('footer')
-        <!--footer started-->
-        <section class="footer">
-            <footer class="bg-gray-4">
-                <p class="footer-text text-white no-mgr-bottom">All Rights Reserved 2018 Planstin, Inc.</p>
-            </footer>
-        </section>
-        <!--footer end-->
-    @show
-
+    @yield('footer')
 
 </body>
 </html>
--- resources/views/layouts/portal.blade.php
+++ resources/views/layouts/portal.blade.php
@@ -49,6 +49,16 @@
 
 @endsection
 
+@section('footer')
+    <!--footer started-->
+    <section class="footer">
+        <footer class="bg-gray-4">
+            <p class="footer-text text-white no-mgr-bottom">All Rights Reserved 2018 Planstin, Inc.</p>
+        </footer>
+    </section>
+    <!--footer end-->
+@endsection
+
 @section('container')
 
     <div class="row">
